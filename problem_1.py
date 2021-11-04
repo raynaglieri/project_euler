33f7e3416bb51c9abb371569e953d32d933b0b4e
Ray Naglieri <raymondnaglieri@gmail.com>
2021-11-04 00:53:59 -0400

    sol completed.

--- problem_1.py
+++ problem_1.py
@@ -15,20 +15,20 @@ def sol():
 
     # Get bound for sum
     def sum_bound(n):
-        return floor(MULTIPLE_MAX - 1/n)
-
+        bound = floor((MULTIPLE_MAX - 1) / n)
+        return bound
     # closed sum form for for first n numbers
     def sum_n(n):
-        return (n * (n+1) / 2)
+        return ((n) * (n+1)) / 2
 
-    sum = 0
+    total = 0
     common_multiple = 1
 
     # Get total sum for all seeds
     for seed in SEEDS:
-        # since 3 is a shared divisor we can remove it from the sum
+        # since seed is a shared divisor we can remove it from the sum
         common_multiple *= seed
-        sum += seed * sum_n(sum_bound(seed))
+        total += seed * sum_n(sum_bound(seed))
 
     # we dont want duplicates
     # i.e 3: 3 * 5 or 5: 5 * 3
@@ -36,9 +36,12 @@ def sol():
     # so we want to calculate the multiples of p1 * p2 less than multiple max
     # for non-prime we need to remove shared multiples n1 <= duplicates <= n2
     duplicates = common_multiple * sum_n(sum_bound(common_multiple))
-    sol = int(sum - duplicates)
+    sol = int(total - duplicates)
 
     print(sol)
 
+
+
 sol()
-        
+
+
